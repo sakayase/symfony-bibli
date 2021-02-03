d959623626eecb527207b2afc1e6b5566fd99165
Saka <simon.ponitzki@gmail.com>
2021-02-03 15:43:17 +0100

    role : choice type

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -13,7 +14,14 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'administrateur' => 'ROLE_ADMIN',
+                    'utilisateur' => 'ROLE_USER',    
+                    ],
+                'multiple' => true,
+                'expanded' => true,
+                ])
             ->add('password')
         ;
     }
